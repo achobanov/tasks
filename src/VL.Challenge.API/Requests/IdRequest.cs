7aa3339dc137fae20a32aee5874253236c6a82d0
achobanov <alexchobanov@gmail.com>
2024-01-09 22:00:47 +0200

    Remove stuff unecessary for Toni's task

--- src/VL.Challenge.API/Requests/IdRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace VL.Challenge.API.Requests;
-
-public class IdRequest
-{
-    [FromRoute(Name = "id")]
-    public int Id { get; set; }
-}
--- start.bat
+++
@@ -1,7 +0,0 @@
-docker compose up -d --build
-
-dotnet tool install --global dotnet-ef --version 7.0.14
-
-cd src/VL.Challenge.API
-dotnet ef database update --project ../VL.Challenge.Storage -- localhost
-cd ../..
