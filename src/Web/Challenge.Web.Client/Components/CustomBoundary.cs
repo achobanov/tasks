a2bb08c7721fee7a8df9924ffe6eac4749053422
achobanov <alexchobanov@gmail.com>
2024-06-27 21:57:26 +0300

    Add basic error boundary for client

---
+++ src/Web/Challenge.Web.Client/Components/CustomBoundary.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+
+namespace Challenge.Web.Client.Components;
+
+public class CustomBoundary : ErrorBoundary
+{
+    public string? Message => CurrentException?.Message;
+    public string? StackTrace => CurrentException?.StackTrace;
+}
