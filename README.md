b8b49d703f722a3dd726a885e530315da8183221
achobanov <alexchobanov@gmail.com>
2024-07-02 16:49:52 +0300

    Upload short documentation in README

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Solution
 Alex's crack at the challenge
+
+## Documentation
+The application runs in Docker. Start it using the following command:
+```
+docker compose up -d
+```
+This will spin up a container on port:8000. The server is Dotnet 8 ASP.NET Core API, which also serves the client Blazor app. On the home page there is an upload button which can be used to send one or more files to the server app.
+
+### API
+#### POST file/upload 
+```json
+{
+    "files:
+    [
+        { 
+            "name": "some-name",
+            "content": "content encoded as base64 string",
+            "encodingName": "utf-8"
+        }
+    ]
+}
+```
--- src/Web/Challenge.Web.API/Controllers/FileController.cs
+++ src/Web/Challenge.Web.API/Controllers/FileController.cs
@@ -11,26 +11,6 @@ public class FileController : ControllerBase
 {
     private readonly INotifier _notifier;
     private readonly IUploadDispatcher _uploadDispatcher;
-    private List<string> _testFilenames = new List<string>
-    {
-        @"../../somefile.txt",
-        @"../../otherfile.exe",
-        @"..\..\thirdfile.json",
-        @"../..\fourthfile.test",
-        @"../../../fifth",
-        @"..\..\..\sixth.exe",
-        @"%CommonProgramFiles%\seventh",
-        @"%PATH%/eight.bat",
-        @"%PATH%/dir/eight.bat",
-        @"~/../../nineth.sh",
-        @"~/tenth.sh",
-        @"/etc/something",
-        @"/etc/bin/something.sh",
-        @"/d/one/two.exe",
-        @"/c/one/three.exe",
-        @"\d\one\four.exe",
-        @"%cd%/file.one"
-    };
 
     public FileController(INotifier notifier, IUploadDispatcher uploadDispatcher)
     {
